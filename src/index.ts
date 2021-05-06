71e1c868e7034ec8b790c56cbe6f53ebdc37e496
tocz9ea <tocz9es@users.noreply.github.com>
2021-05-06 11:58:07 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,14 @@ import {
 import { IThemeManager } from '@jupyterlab/apputils';
 
 /**
- * Initialization data for the @tocz9e/jupyter-theme-clear extension.
+ * Initialization data for the @tocz9e/jupyterlab-theme-clear extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
-  id: '@tocz9es/jupyter-theme-clear:plugin',
+  id: '@tocz9es/jupyterlab-theme-clear:plugin',
   requires: [IThemeManager],
   autoStart: true,
   activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
-    const style = '@tocz9es/jupyter-theme-clear/index.css';
+    const style = '@tocz9es/jupyterlab-theme-clear/index.css';
     manager.register({
       name: 'Clear Theme',
       isLight: true,
